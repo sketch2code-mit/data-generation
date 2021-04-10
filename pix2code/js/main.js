3eab6fe91e372d90d687d5b2402b288b831f5029
Jaeyoon Song <jyo3on@gmail.com>
2021-04-10 00:01:05 -0400

    add pix2code data

--- pix2code/js/main.js
+++ pix2code/js/main.js
@@ -1,8 +1,16 @@
 window.onload = function() {
 	const divs = Array.from(document.querySelectorAll("div"))
 	divs.forEach(function(div, i) {
-		if (div.innerText.trim().length == 0) {
+		let inner = div.innerText.trim();
+		if (inner.length == 0 || inner.includes("img")) {
 			div.style.backgroundColor = "transparent"
 		}
 	})
+
+	let navbar = document.querySelector(".navbar-nav")
+	let container_in_navbar = navbar.querySelector(".container")
+	if (container_in_navbar) {
+		let tmp = container_in_navbar.innerHTML
+		navbar.innerHTML = tmp
+	}
 };
